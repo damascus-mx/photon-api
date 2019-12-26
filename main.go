962ba606be9e0c18f5acb930037a309b77aede70
Alonso Ruiz <luis.alonso.16@hotmail.com>
2019-12-26 16:51:57 -0700

    Add SSL certificates

--- main.go
+++ main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
 	app "github.com/damascus-mx/photon-api/src/bin"
 )
 
 func main() {
-	http.ListenAndServe(":3000", app.InitApplication())
+	err := http.ListenAndServe(":3000", app.InitApplication())
+	// http.ListenAndServeTLS(":443", "./security/localhost.crt", "./security/localhost.key", app.InitApplication())
+	if err != nil {
+		log.Fatal(err.Error())
+	}
 }
