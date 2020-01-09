612b4430f749d5222524ed785f72344adb82f637
Alonso Ruiz <luis.alonso.16@hotmail.com>
2020-01-08 23:55:37 -0700

    Change go build flags for build

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -a -installsuffix cgo -o users ./users/
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -a -installsuffix cgo -o tasks ./tasks/
+go build -o users ./users/
+go build -o tasks ./tasks/
